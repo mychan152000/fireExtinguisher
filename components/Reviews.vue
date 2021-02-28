8142cf0ceb3235bff9eb94538e1396f7199e9ca8
Tran My <mychan@Mychans-Macbook-Pro.local>
2021-02-28 15:49:42 +0700

    commit

---
+++ components/Reviews.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Customer Reviews</h3>
+
+    <ReviewCard
+      v-for="review in reviewers.results"
+      :key="review.login.uuid"
+      :review="review"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      reviewers: []
+    }
+  },
+  async fetch () {
+    this.reviewers = await fetch(
+      'https://randomuser.me/api/?results=5'
+    ).then(res => res.json())
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="container">
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo"
+        :cards-section="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo"
+        :cards-section="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '@/assets/data'
+
+export default {
+  data () {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    }
+  }
+}
+</script>
+
+<style scoped></style>
